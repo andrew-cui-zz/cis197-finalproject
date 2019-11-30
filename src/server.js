f3256738eb1a29ad8a90c47ca7c846ed3ef8ab35
Andrew Yiqun Cui <andrewc@seas.upenn.edu>
2019-11-30 16:41:00 -0500

    add links and data repo + img 2

--- src/server.js
+++ src/server.js
@@ -16,6 +16,7 @@ app.set('port', port)
 app.set('view engine', 'html')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '..', 'public/views')) // set views for ejs
+app.set('json spaces', 2) // json prettify
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(
